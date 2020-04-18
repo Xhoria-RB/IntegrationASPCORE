e95128e39a59c70430866c56be1cd1d3652706ab
Ricardo Bibieca <shirotokuro3715@gmail.com>
2020-04-18 00:20:42 -0400

    First commit

---
+++ VTigerCore/Data/VTigerCoreContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using VTigerCore.Models;
+
+namespace VTigerCore.Models
+{
+    public class VTigerCoreContext : DbContext
+    {
+        public VTigerCoreContext (DbContextOptions<VTigerCoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<VTigerCore.Models.Product> Product { get; set; }
+
+        public DbSet<VTigerCore.Models.User> User { get; set; }
+    }
+}
---
+++ VTigerCore/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VTigerCore.Models
+{
+    public class Product
+    {
+        public string ProductId { get; set; }
+        public string ProductName { get; set; }
+        public bool ProductActive { get; set; }
+        public float UnitPrice { get; set; }
+        public float Taxes { get; set; }
+        public int QTStock { get; set; }
+        public string handler { get; set; }
+
+
+    }
+}
